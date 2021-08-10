d4db2ef5b8b985d5653033bd1f94c4f78c7a369a
Eliad Tuksher <eliad91@gmail.com>
2021-08-10 18:27:26 +0300

    updating the date utilities

--- src/utils/formatSelectDate.ts
+++ src/utils/formatSelectDate.ts
@@ -1,10 +1,32 @@
 import format from 'date-fns/format';
 
-export default function formatSelectedDate(date: string) {
-  const [dateString] = date.split('_');
-  const year = +dateString.substring(0, 4);
-  const month = +dateString.substring(4, 6);
-  const day = +dateString.substring(6, 8);
+function extractPreciseDateUnits(fullString: string, by: 'date' | 'time'): Record<string, number> {
+  let first = 0,
+    second = 0,
+    third = 0;
 
-  return format(new Date(year, month, day), 'dd/MM/yyyy');
+  if (by === 'date') {
+    first = +fullString.substring(0, 4);
+    second = +fullString.substring(4, 6);
+    third = +fullString.substring(6, 8);
+  } else if (by === 'time') {
+    first = +fullString.substring(0, 2);
+    second = +fullString.substring(2, 4);
+    third = +fullString.substring(4, 6);
+  }
+
+  return { first, second, third };
+}
+
+export default function formatSelectedDate(date: string) {
+  const [dateString, timeString] = date.split('_');
+
+  const { first: year, second: month, third: day } = extractPreciseDateUnits(dateString, 'date');
+  const {
+    first: hour,
+    second: minute,
+    third: second,
+  } = extractPreciseDateUnits(timeString, 'time');
+
+  return format(new Date(year, month, day, hour, minute, second), 'dd/MM/yyyy hh:mm:dd a');
 }
